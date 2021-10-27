6e12feab6b12adc16fbebef9cabfa5e93ed9c50f
Doug <dev@baytechdesign.com.au>
2021-10-27 18:22:17 +1030

    Finished crew, added nav bar to all pages and formatted HTML in places.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+  
+function displayPat() {
+  var x = document.getElementById("pat");
+  var y = document.getElementById("matt");
+	var z = document.getElementById("cody");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+	  y.style.display = "none";
+	  z.style.display = "none";
+  } else {
+	  x.style.display = "none";
+	  
+  }
+}
+
+function displayMatt() {
+  var x = document.getElementById("matt");
+	var y = document.getElementById("pat");
+	var z = document.getElementById("cody");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+	  y.style.display = "none";
+	  z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function displayCody() {
+  var x = document.getElementById("cody");
+	var y = document.getElementById("pat");
+	var z = document.getElementById("matt");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+	  y.style.display = "none";
+	  z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
